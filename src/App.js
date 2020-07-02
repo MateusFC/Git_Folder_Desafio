2fd8082bcde2e7cab7c73d235e337a934346eaed
MateusFC <fontinelemateus@gmail.com>
2020-07-02 11:48:34 -0300

    subindo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Home from './Home'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path='/' component={Home} />
-				<Route path='/:user' component={Home} />
+				<Redirect path='/:user' component={Home} />
 			</Switch>
 		</BrowserRouter>
 	)
